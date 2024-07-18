715bf044659f5cf72f5bf8d1d2fcafe8aa90ce15
璟义 <yangshangpo.ysp@alibaba-inc.com>
2024-07-18 22:53:59 +0800

    optimize qa

--- chat/chat.py
+++ chat/chat.py
@@ -12,13 +12,13 @@ def call_with_messages(retrieval_contents: List[str], question: str) -> str:
         msg_content = PROMPT_TEMPLATES.get('default').replace('{{ context }}', '\n'.join(retrieval_contents)).replace('{{ question }}', question)
     else:
         msg_content = PROMPT_TEMPLATES.get('Empty').replace('{{ question }}', question)
+
+    messages = [Message(role=Role.SYSTEM, content='You are a helpful assistant.'),
+                Message(role=Role.USER, content=msg_content)]
     if len(st.session_state.qa_history) == 0:
-        messages = [Message(role=Role.SYSTEM, content='You are a helpful assistant.'),
-                    Message(role=Role.USER, content=msg_content)]
         st.session_state.qa_history = messages.copy()
     else:
         st.session_state.qa_history.append(Message(role=Role.USER, content=msg_content))
-        messages = st.session_state.qa_history.copy()
     response = dashscope.Generation.call(
         dashscope.Generation.Models.qwen_turbo,
         messages=messages,
@@ -30,7 +30,8 @@ def call_with_messages(retrieval_contents: List[str], question: str) -> str:
         # append result to history.
         st.session_state.qa_history.append(
             Message(role=response.output.choices[0]['message']['role'],
-                    content=response.output.choices[0]['message']['content'])
+                    content=response.output.choices[0]['message']['content'],
+                    image_urls=None)
         )
         return '\n\n'.join([i.message.content for i in response.output.choices])
     else:
--- utils/document_utils/qa.py
+++ utils/document_utils/qa.py
@@ -2,12 +2,29 @@ import streamlit as st
 from chat import *
 from .document_search import do_query_content
 from utils.utils import write_error
+import json
 
 
 def parse_question_from_qa_history(content: str):
     if '<问题>' not in content:
         return content
     return content[content.index('<问题>')+4:content.index('</问题>')]
+
+def get_image_urls(retrieval_contents):
+    image_urls = []
+    for data in retrieval_contents:
+        if data.loader_metadata:
+            loader_metadata = json.loads(data.loader_metadata)
+            if 'image_refs' in loader_metadata:
+                for image_ref in loader_metadata['image_refs']:
+                    if image_ref.get('img_pos') == 'front':
+                        image_urls.append(loader_metadata['image_ref_urls'][
+                                     f'{image_ref["page_index"]}_{image_ref["img_index"]}'])
+                for image_ref in loader_metadata['image_refs']:
+                    if image_ref.get('img_pos') == 'behind':
+                        image_urls.append(loader_metadata['image_ref_urls'][
+                                     f'{image_ref["page_index"]}_{image_ref["img_index"]}'])
+    return list(set(image_urls))
 
 
 def do_qa(question_content, top_k, filter_str, metrics, use_full_text_retrieval):
@@ -18,6 +35,9 @@ def do_qa(question_content, top_k, filter_str, metrics, use_full_text_retrieval)
                     st.write(parse_question_from_qa_history(h.content))
                 else:
                     st.write(h.content)
+                    if h.image_urls:
+                        for u in h.image_urls:
+                            st.image(u)
     with st.chat_message("user"):
         st.write(question_content)
     try:
@@ -25,13 +45,18 @@ def do_qa(question_content, top_k, filter_str, metrics, use_full_text_retrieval)
         answer = call_with_messages([i.content for i in retrieval_contents], question_content)
         with st.chat_message("assistant"):
             st.write(answer)
+            image_urls = get_image_urls(retrieval_contents)
+            if len(image_urls) > 0:
+                for u in image_urls:
+                    st.image(u)
+                st.session_state.qa_history[-1].image_urls = image_urls
     except Exception as e:
         write_error(str(e))
 
 
 def qa_page(question):
     with st.expander('高级选项'):
-        top_k = st.number_input('TopK:', value=10)
+        top_k = st.number_input('TopK:', value=3)
         filter_str = st.text_area('Filter:')
         metrics = st.selectbox('Metrics:', options=[None, 'cosine', 'l2', 'ip'])
         use_full_text_retrieval = st.toggle('是否全文检索', value=True)
